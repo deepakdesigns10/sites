ce81d40a0f63bda5de266250f4d217665cd7ee7e
thedteam <deepakdesigns10@gmail.com>
2026-09-04 11:53:02 +0530

    Update popup-injector.js

--- popup-injector.js
+++ popup-injector.js
@@ -1,7 +1,7 @@
 (function() {
     // Set the expiration date (YYYY-MM-DDTHH:MM:SS format).
-    // Example: '2026-08-22T00:00:00' disables it at exactly 12:00 AM on August 22th.
-    const expirationDate = new Date('2026-08-22T00:00:00'); 
+    // Example: '2026-09-05T00:00:00' disables it at exactly 12:00 AM on September 05th.
+    const expirationDate = new Date('2026-09-06T00:00:00'); 
     if (new Date() >= expirationDate) {
         return; // Exit the script entirely; the popup will not be injected or shown
     }
@@ -102,7 +102,7 @@
             <div class="dd-modal-container">
                 <button class="dd-modal-close" id="centralCloseBtn">&times;</button>
                 <a href="https://www.thedteam.in" target="_blank" class="dd-modal-link">
-                    <img class="dd-modal-image" src="https://scontent.fblr20-4.fna.fbcdn.net/v/t39.30808-6/782413183_1492475976231953_4510760535366811714_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x2048&ctp=s2048x2048&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=kp-5ZNoXOVwQ7kNvwHt7uXu&_nc_oc=Adqu16a1YvTqcaYbsp2wm7DFu5fuSLCFhBmNyUaMT_PNOz2CNVfCOd2CLQ87Xd4bDg6le3HIQ9ynyv0RvIcFStXU&_nc_zt=23&_nc_ht=scontent.fblr20-4.fna&_nc_gid=zjJXhCDjhDWnfOMGC3DjLg&_nc_ss=7b2a8&oh=00_AQGw7Oi84E2VtEL0dz7kklbnRvnwsurtUjJQqFoTXbll0A&oe=6A8DAA1B" alt="Special Offer" draggable="false" oncontextmenu="return false;">
+                    <img class="dd-modal-image" src="https://scontent.fblr4-3.fna.fbcdn.net/v/t51.82787-15/795767247_18489769093099348_4513946323237379656_n.webp?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=N44ipYkmEvYQ7kNvwHPCofR&_nc_oc=AdpTtCUW3ou0uFTzMa9UU6QNltXskLkcjdtQQdunDGrW8LLLJe4QuciVT7jiRyDW3iU_-7pV0Qr1nw78CnfAXZ3i&_nc_zt=23&_nc_ht=scontent.fblr4-3.fna&_nc_gid=NcD_KXhXKM004aVXHXGYbQ&_nc_ss=7b2a8&oh=00_AQLYwaI9Q_3bS7JYEvX30g-TumMvLdi5vroJdZSckPIswg&oe=6AA01E57" alt="Special Offer" draggable="false" oncontextmenu="return false;">
                     <img class="dd-watermark" src="https://client.thedteam.in/assets/images/favicon.png" alt="Watermark" draggable="false" oncontextmenu="return false;">
                 </a>
             </div>
