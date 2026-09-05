9832edf72391f844fec9d48e473779ea01fc36cf
thedteam <deepakdesigns10@gmail.com>
2026-09-05 07:57:45 +0530

    Update popup-injector.js

--- popup-injector.js
+++ popup-injector.js
@@ -107,7 +107,7 @@
             <div class="dd-modal-container">
                 <button class="dd-modal-close" id="centralCloseBtn">&times;</button>
                 <a href="https://www.thedteam.in" target="_blank" class="dd-modal-link">
-                    <img class="dd-modal-image" src="https://scontent.fblr4-3.fna.fbcdn.net/v/t51.82787-15/795767247_18489769093099348_4513946323237379656_n.webp?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=N44ipYkmEvYQ7kNvwHPCofR&_nc_oc=AdpTtCUW3ou0uFTzMa9UU6QNltXskLkcjdtQQdunDGrW8LLLJe4QuciVT7jiRyDW3iU_-7pV0Qr1nw78CnfAXZ3i&_nc_zt=23&_nc_ht=scontent.fblr4-3.fna&_nc_gid=NcD_KXhXKM004aVXHXGYbQ&_nc_ss=7b2a8&oh=00_AQLYwaI9Q_3bS7JYEvX30g-TumMvLdi5vroJdZSckPIswg&oe=6AA01E57" alt="Special Offer" draggable="false" oncontextmenu="return false;">
+                    <img class="dd-modal-image" src="https://scontent.fblr20-4.fna.fbcdn.net/v/t39.30808-6/796808911_979585878455155_1964759888164225632_n.jpg?stp=dst-jpg_tt6&cstp=mx1024x1024&ctp=s1024x1024&_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=XdPvxiiElWUQ7kNvwFFAriT&_nc_oc=AdphVrju8Th7s4QYJY5yuol8zyjkJ8vhxPaPQ4puLA9CdmOL2wxzmUxNwx9HmcKXWay1upoh4K0-0w9aAq2T8SH_&_nc_zt=23&_nc_ht=scontent.fblr20-4.fna&_nc_gid=Pr3E1AndLa2WVJkbgIESxg&_nc_ss=7b2a8&oh=00_AQImo50qGum2zUWfKY0-BX4Kx3BXzPeNFUqJsNx442zPAQ&oe=6AA1485A" alt="Special Offer" draggable="false" oncontextmenu="return false;">
                     <img class="dd-watermark" src="https://client.thedteam.in/assets/images/favicon.png" alt="Watermark" draggable="false" oncontextmenu="return false;">
                 </a>
             </div>
